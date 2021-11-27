7df069b44c887610503b8c3446d4dc372e590a23
fedealliani <fedealliani@gmail.com>
2021-11-27 13:52:02 -0300

    fix candlestrategy

--- strategies/CandleStrategy/service.go
+++ strategies/CandleStrategy/service.go
@@ -10,8 +10,8 @@ func (c *CandleStrategy) ShouldBuy(info bot.Info) (bool, float64, error) {
 	candlesInfo := GetInfoFromCandles(info.Candles, c.AmountCandles)
 	fmt.Println(candlesInfo)
 	if candlesInfo.AmountRedCandles == c.AmountCandles {
-		otherCoinAmount := c.TradingAmount * info.Candles[len(info.Candles)-1].Close
-		if otherCoinAmount < info.AmountOtherCoin {
+		tradeAmountWithComission := c.TradingAmount * (1 + info.Comission)
+		if tradeAmountWithComission < info.AmountOtherCoin {
 			return true, c.TradingAmount, nil
 		} else {
 			fmt.Println("Cannot buy because don´t have other coin to buy")
@@ -24,10 +24,12 @@ func (c *CandleStrategy) ShouldBuy(info bot.Info) (bool, float64, error) {
 
 func (c *CandleStrategy) ShouldSell(info bot.Info) (bool, float64, error) {
 	candlesInfo := GetInfoFromCandles(info.Candles, c.AmountCandles)
+	fmt.Println(candlesInfo)
 	if candlesInfo.AmountGreenCandles == c.AmountCandles {
-
-		if c.TradingAmount < info.AmountDominantCoin {
-			return true, c.TradingAmount, nil
+		tradeAmount := c.TradingAmount / info.LastPrice
+		tradeAmountWithComission := tradeAmount * (1 + info.Comission)
+		if tradeAmountWithComission < info.AmountDominantCoin {
+			return true, tradeAmount, nil
 		} else {
 			fmt.Println("Cannot sell because don´t have dominant coin to sell")
 		}
